5f9b6672a36d043d4b7f674a0edeb5ca7b63dd73
jujujuujuu <98986454+jujujuujuu@users.noreply.github.com>
2023-10-20 21:54:45 +0000

    e

---
+++ entrega/guia-1/uno.py
@@ -0,0 +1,28 @@
+# a)
+print("Las maquinas me sorprenden con mucha frecuencia")
+
+# b)
+print("")
+
+# c)
+print("i love cheebugers")
+
+# d) RTA: No hay diferencia entre los números en la terminal
+print(23, "23")
+
+# e)
+print('Una computadora puede ser llamada "inteligente" si logra engañar a una persona haciéndole creer que es un humano.')
+
+# f) y g)
+print("Ethan", "Butterfield", "15", sep="_")
+
+# h) e i)
+print("calle\t \n número \n \t código postal")
+
+# j)
+str = "Feliz\t \n Primavera \n \t 2023."
+print(str)
+
+# k)
+print("Solo podemos ver poco del futuro", end=", ")
+print("pero lo suficiente para darnos cuenta de que hay mucho que hacer")
