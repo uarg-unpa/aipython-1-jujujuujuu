91a38b3fa0c76c821e193ec68f6ad076d7be4155
jujujuujuu <98986454+jujujuujuu@users.noreply.github.com>
2023-11-02 22:30:09 +0000

    Actualizo

---
+++ entrega/guia-1/cinco.py
@@ -0,0 +1,8 @@
+print("Hola! Para ingresar, a continuación debera ingresar su nombre:")
+nombre = input()
+print("Ahora, ingrese su apellido:")
+apellido = input()
+print("Ya casi! ahora ingrese su edad:")
+edad = input()
+
+print("\n Listo! Ahora,", nombre, apellido, "ústed tendrá la mejor experiencia de sus", edad, "años de vida!")
