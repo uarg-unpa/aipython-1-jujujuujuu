e2b6fb8fa0cc241a0429d3b198286756373ac095
jujujuujuu <98986454+jujujuujuu@users.noreply.github.com>
2023-11-02 22:22:01 +0000

    Actualizo

---
+++ entrega/guia-1/tres.py
@@ -0,0 +1,11 @@
+mi_nombre  = "Ethan"
+mi_apellido = "Butterfield"
+mi_edad = 15
+mi_altura = 1.67
+
+n_vuelo = 2314
+temp_ambiente = 22.1
+salario_mensual = 180000
+
+juego_activo = True
+es_par = False
