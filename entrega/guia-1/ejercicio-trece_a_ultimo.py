60cd2185581b9458abbe33bc0e483ace70ec2fe5
jujujuujuu <98986454+jujujuujuu@users.noreply.github.com>
2023-11-03 01:24:37 +0000

    Actualizo

---
+++ entrega/guia-1/ejercicio-trece_a_ultimo.py
@@ -0,0 +1,85 @@
+# 13)
+item1 = 25
+item2 = 64
+item3 = 10
+item4 = 241
+item5 = 10
+item6 = 15
+item7 = 60
+item8 = 3
+item9 = 102
+item10 = 1
+
+promedio = (item1+item2+item3+item4+item5+item6+item7+item8+item9+item10)/10
+print(promedio)
+
+# 14)
+print("\nUna ambiciosa "+"Introducción "+"a Python "+"Parte 1")
+
+# 15)
+sociedad = 'aiPython P1'
+print("")
+print(sociedad)
+print(len(sociedad))
+print(sociedad.upper())
+print(sociedad.lower())
+
+# 16)
+txt = "sometimes it is the people no one imagines anything of who do the things that no one can imagine."
+print("")
+print(txt.capitalize())
+print(txt.title())
+print(txt.swapcase())
+
+# 17)
+print("")
+print(input("Nombre completo: ")*3)
+
+# 18)
+print("       *")
+print("      * *")
+print("     *   *")
+print("    *     *")
+print("   *       *")
+print("  *         *")
+print(" *           *")
+print("*****     *****")
+print("    *     *")
+print("    *     *")
+print("    *     *")
+print("    *     *")
+print("    *******")
+
+# 19)
+print("       *\n      * *\n     *   *\n    *     *\n   *       *\n  *         *\n *           *\n*****     *****\n    *     *\n    *     *\n    *     *\n    *     *\n    *******")
+
+# 20)
+print("       *                *")
+print("      * *              * *")
+print("     *   *            *   *")
+print("    *     *          *     *")
+print("   *       *        *       *")
+print("  *         *      *         *")
+print(" *           *    *           *")
+print("*****     *****  *****     *****")
+print("    *     *          *     *")
+print("    *     *          *     *")
+print("    *     *          *     *")
+print("    *     *          *     *")
+print("    *******          *******")
+
+# 21)
+print(input("Ingrese una palabra: ").replace("a", "☺"))
+
+# 22) y 24) (no terminado)
+print("")
+print("El razonamiento matemático puede considerarse más bien esquemáticamente como el ejercicio de una combinación de dos instalaciones, que podemos llamar la intuición y el ingenio")
+print("El razonamiento matemático puede considerarse más bien esquemáticamente como el ejercicio de una combinación de\ndos instalaciones, que podemos llamar la intuición y el ingenio")
+
+# 23)
+print("")
+print("   La ciencia es una ecuación diferencial. La religión es una condición de frontera.   "[3:84])
+
+# 25)
+print("")
+print("\tNombre\tEdad\tPais\tCiudad\n\tAlexa\t250\tUSA\tCapeCod")
