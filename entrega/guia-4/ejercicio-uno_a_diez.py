e1cd228d9abd914f53aac735ef7ebe72202c753e
jujujuujuu <98986454+jujujuujuu@users.noreply.github.com>
2023-11-13 19:55:59 +0000

    Actualizo Guia 4

---
+++ entrega/guia-4/ejercicio-uno_a_diez.py
@@ -0,0 +1,23 @@
+# 1) a 3)
+lista_vacia = []
+lista1 = [1,3,4,5,6,7,8]
+print(len(lista1))
+
+# 4)
+frutas_favoritas = ["Manzana", "Durazno", "Banana", "Ananá"]
+print(len(frutas_favoritas))
+frutas_favoritas.pop(0)
+print(frutas_favoritas)
+frutas_favoritas.append("Manzana")
+print(frutas_favoritas)
+
+# 5)
+longitud = len(lista1)-1
+print(lista1[0], lista1[int(longitud/2)], lista1[longitud])
+
+# 6)
+datos_personales = []
+datos_personales.append("Ethan Butterfield")
+datos_personales.append(15)
+datos_personales.append(1.62)
+datos_personales.append("Rio Gallegos, Santa Cruz, Argentina")
